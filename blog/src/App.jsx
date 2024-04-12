89bf63943c97dc7cecd996f97f71ed4a176bce10
Preetam Shri <preetamsshri@gmail.com>
2024-04-13 00:47:14 +0530

    Setup redux  in the project

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,6 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/authService";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <h1>A blog app with Appwrite</h1>
