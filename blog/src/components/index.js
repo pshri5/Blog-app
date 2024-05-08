75e2ab2b478bc3ef64ed37ae616b1feea0b05945
Preetam Shri <preetamsshri@gmail.com>
2024-05-08 20:30:45 +0530

    Update index.js
    Add Input and Select component which was missing

--- blog/src/components/index.js
+++ blog/src/components/index.js
@@ -9,6 +9,8 @@ import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
 
 export {
   Header,
@@ -22,4 +24,6 @@ export {
   PostForm,
   PostCard,
   AuthLayout,
+  Input,
+  Select,
 };
