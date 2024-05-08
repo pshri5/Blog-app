d9ed7a7a662bda4aeccfcfd3c13845485e2d8aca
Preetam Shri <preetamsshri@gmail.com>
2024-05-08 21:58:45 +0530

    Update main.jsx

--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -8,13 +8,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
 
-import AddPost from "./pages/AddPost";
-import Signup from "./pages/Signup";
-import EditPost from "./pages/EditPost";
+import AddPost from "./pages/AddPost.jsx";
+import Signup from "./pages/Signup.jsx";
+import EditPost from "./pages/EditPost.jsx";
 
-import Post from "./pages/Post";
+import Post from "./pages/Post.jsx";
 
-import AllPosts from "./pages/AllPosts";
+import AllPosts from "./pages/AllPost.jsx";
 
 const router = createBrowserRouter([
   {
